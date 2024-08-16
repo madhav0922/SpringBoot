aee2465dd4338334b0637e1a36d042c3ac3ba80a
madhav0922 <madhav.sharma0922@gmail.com>
2024-08-16 14:18:25 +0530

    Cleaning all changes before starting next module, formed branches.

--- Initial_Projects/NoBS/src/main/java/com/example/demo/Command.java
+++ Initial_Projects/NoBS/src/main/java/com/example/demo/Command.java
@@ -7,6 +7,6 @@ public interface Command <E, T> {
     // <E, T> = Entity, T is generic type in java
 
     ResponseEntity <T> execute(E entity);
-    // we can also name the method handle.. since its a handler.. but execute is also used..
+    // we can also name the method handle... since its a handler... but execute is also used...
 
 }
